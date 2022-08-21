b92f4bfb4148b6261647b55a5adec909bffd31d4
bryanemmanuel <bryan.emmanuel.paz@gmail.com>
2022-08-21 18:34:07 +0800

    update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,30 @@ const Hero = () => {
     <div className="hero">
             <div className="left-h">
                 <Header/>
+
+
+                <div className="the-best-ad">
+                    <div>
+                    </div>
+                    <span> the best fitness club in the town</span>
+                </div>
+
+
+                <div className="hero-text">
+                    <div>
+                        <span>Shape</span>
+                        <span>Your</span>
+                    </div>
+
+                    <div>
+                        <span>Ideal body</span>
+                    </div>
+                </div>
             </div>
+           
+           
+           
+           
             <div className="right-h">right side</div>
     </div>
   )
