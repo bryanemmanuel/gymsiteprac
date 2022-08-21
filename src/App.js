c27e0225817eea42bf2dbb93b402a855565dac43
bryanemmanuel <bryan.emmanuel.paz@gmail.com>
2022-08-21 17:13:43 +0800

    up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Hero } from './components';
+import './App.css'
+const App = () => {
+  return (
+    <div className="App">
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
